dd268d9448c630c3d6f5090f18dd0dad89de8a15
colamax <colamax@foxmail.com>
2014-05-04 09:59:45 +0800

    test

--- UserAgent.Show/Program.cs
+++ UserAgent.Show/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO; 
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using UserAgent;
@@ -12,14 +14,49 @@ namespace UserAgent.Show
         static void Main(string[] args)
         {
             UserAgentParser uap = new UserAgentParser();
-            uap.OnProcessUnknowUa += new ProcessUnknowUaEventHandler(uap_OnProcessUnknowUa);
-			TerminalModel tm = uap.ParseUserAgent("DOOjjjjjjjjj-cn");
-            Console.WriteLine("tm.Browser=" + tm.Browser);
-            Console.WriteLine("tm.Brand=" + tm.Brand);
-            Console.WriteLine("tm.Model=" + tm.Model);
-            Console.WriteLine("tm.Platform=" + tm.Platform);
-            Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-            Console.ReadKey();
+//            uap.OnProcessUnknowUa += new ProcessUnknowUaEventHandler(uap_OnProcessUnknowUa);
+//			TerminalModel tm = uap.ParseUserAgent("DOOjjjjjjjjj-cn");
+//            Console.WriteLine("tm.Browser=" + tm.Browser);
+//            Console.WriteLine("tm.Brand=" + tm.Brand);
+//            Console.WriteLine("tm.Model=" + tm.Model);
+//            Console.WriteLine("tm.Platform=" + tm.Platform);
+//            Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+			string strReadFilePath = @"../../part-r-00000(6).txt";
+			StreamReader srReadFile = new StreamReader(strReadFilePath);
+			float total = 0;
+			float haveNum = 0;
+			while (!srReadFile.EndOfStream)
+			{
+				string strReadLine = srReadFile.ReadLine(); //读取每行数据
+				if (strReadLine != null) 
+				{
+					total++;
+					Stopwatch sw = new Stopwatch();
+					sw.Start();
+					TerminalModel tm = uap.ParseUserAgent (strReadLine);
+					sw.Stop();
+					if (tm.Platform != null && tm.Platform.Length>1) {
+						haveNum++;
+//						Console.WriteLine (strReadLine); //屏幕打印每行数据
+//						Console.WriteLine ("tm.Browser=" + tm.Browser);
+//						Console.WriteLine ("tm.Brand=" + tm.Brand);
+//						Console.WriteLine ("tm.Model=" + tm.Model);
+//						Console.WriteLine ("tm.Platform=" + tm.Platform);
+//						Console.WriteLine ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+//						Console.WriteLine("++++>" + sw.ElapsedMilliseconds);
+					} else {
+						Console.WriteLine (strReadLine); //屏幕打印每行数据
+//						Console.WriteLine("---->" + sw.ElapsedMilliseconds);
+					}
+				}
+
+			}
+			//关闭读取流文件
+			srReadFile.Close();
+			Console.WriteLine("total:" + total);
+			Console.WriteLine("haveNum:" + haveNum);
+			Console.WriteLine("PER:" + haveNum/total*100+"%");
         }
 
         static void uap_OnProcessUnknowUa(string uaStr)
