f28ece44ede489cb8deb44dce0acdef0bd1c27a2
colamax <colamax@foxmail.com>
2014-05-06 10:34:29 +0800

    DE

--- UserAgent.Show/Program.cs
+++ UserAgent.Show/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.IO; 
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -14,112 +14,80 @@ namespace UserAgent.Show
         static void Main(string[] args)
         {
             UserAgentParser uap = new UserAgentParser();
-//            uap.OnProcessUnknowUa += new ProcessUnknowUaEventHandler(uap_OnProcessUnknowUa);
-			TerminalModel tm = uap.ParseUserAgent("UCWEB/2.0 (Linux; U; Adr 2.3.4; zh-CN; HTC Sensation XE with Beats Audio Z715e) U2/1.0.0 UCBrowser/9.7.5.418 U2/1.0.0 Mobile");
-            Console.WriteLine("tm.Browser=" + tm.Browser);
-            Console.WriteLine("tm.Brand=" + tm.Brand);
-            Console.WriteLine("tm.Model=" + tm.Model);
-            Console.WriteLine("tm.Platform=" + tm.Platform);
-            Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-            Console.ReadKey();
-//            string strReadFilePath = @"../../part-r-00000(6).txt";
-//            StreamReader srReadFile = new StreamReader(strReadFilePath);
-//            float total = 0;
-//            float haveNum = 0;
-//            while (!srReadFile.EndOfStream)
-//            {
-//                string strReadLine = srReadFile.ReadLine(); //读取每行数据
-//                if (strReadLine != null) 
-//                {
-//                    total++;
-//                    Stopwatch sw = new Stopwatch();
-//                    sw.Start();
-//                    TerminalModel tm = uap.ParseUserAgent (strReadLine);
-//                    sw.Stop();
-//                    if (tm.Platform != null && tm.Platform.Length>1) {
-//                        haveNum++;
-////						Console.WriteLine (strReadLine); //屏幕打印每行数据
-////						Console.WriteLine ("tm.Browser=" + tm.Browser);
-////						Console.WriteLine ("tm.Brand=" + tm.Brand);
-////						Console.WriteLine ("tm.Model=" + tm.Model);
-////						Console.WriteLine ("tm.Platform=" + tm.Platform);
-////						Console.WriteLine ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-////						Console.WriteLine("++++>" + sw.ElapsedMilliseconds);
-//                    } else {
-//                        Console.WriteLine (strReadLine); //屏幕打印每行数据
-////						Console.WriteLine("---->" + sw.ElapsedMilliseconds);
-//                    }
-//                }
+            //            uap.OnProcessUnknowUa += new ProcessUnknowUaEventHandler(uap_OnProcessUnknowUa);
+            //			TerminalModel tm = uap.ParseUserAgent("DOOjjjjjjjjj-cn");
+            //            Console.WriteLine("tm.Browser=" + tm.Browser);
+            //            Console.WriteLine("tm.Brand=" + tm.Brand);
+            //            Console.WriteLine("tm.Model=" + tm.Model);
+            //            Console.WriteLine("tm.Platform=" + tm.Platform);
+            //            Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 
-<<<<<<< HEAD
-//            }
-//            //关闭读取流文件
-//            srReadFile.Close();
-//            Console.WriteLine("total:" + total);
-//            Console.WriteLine("haveNum:" + haveNum);
-//            Console.WriteLine("PER:" + haveNum/total*100+"%");
-=======
-			string strReadFilePath = @"../../part-r-00000(6).txt";
-			StreamReader srReadFile = new StreamReader(strReadFilePath);
-			float total = 0;
-			float haveNum = 0;
-			float ucUser = 0;
-			int ucnum = 0;
-			int allnum = 0;
-			while (!srReadFile.EndOfStream)
-			{
-				string strReadLine = srReadFile.ReadLine(); //读取每行数据
-				if (strReadLine != null) 
-				{
-					total++;
-					Stopwatch sw = new Stopwatch();
-					sw.Start();
-					TerminalModel tm = uap.ParseUserAgent (strReadLine);
-					sw.Stop();
-					if (tm.Platform != null && tm.Platform.Length>1) {
-						haveNum++;
+            string strReadFilePath = @"../../part-r-00000(6).txt";
+            StreamReader srReadFile = new StreamReader(strReadFilePath);
+            float total = 0;
+            float haveNum = 0;
+            float ucUser = 0;
+            int ucnum = 0;
+            int allnum = 0;
+            while (!srReadFile.EndOfStream)
+            {
+                string strReadLine = srReadFile.ReadLine(); //读取每行数据
+                if (strReadLine != null)
+                {
+                    total++;
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    TerminalModel tm = uap.ParseUserAgent(strReadLine);
+                    sw.Stop();
+                    if (tm.Platform != null && tm.Platform.Length > 1)
+                    {
+                        haveNum++;
 
-						Console.WriteLine (strReadLine); //屏幕打印每行数据
-						string[] sArray = strReadLine.Split ('\t');
-						if (sArray [1].Length > 0) {
-							int result=int.Parse(sArray [1].ToString());
-							Console.WriteLine("ALL+"+result);
-							allnum += result;
-						}
+                        Console.WriteLine(strReadLine); //屏幕打印每行数据
+                        string[] sArray = strReadLine.Split('\t');
+                        if (sArray[1].Length > 0)
+                        {
+                            int result = int.Parse(sArray[1].ToString());
+                            Console.WriteLine("ALL+" + result);
+                            allnum += result;
+                        }
 
-						if(tm.Browser.Equals("UCBrowser")){
-							ucUser++;
-							Console.WriteLine (strReadLine); //屏幕打印每行数据
-							sArray = strReadLine.Split ('\t');
-							if (sArray [1].Length > 0) {
-								int result=int.Parse(sArray [1].ToString());
-								Console.WriteLine("UC+"+result);
-								ucnum += result;
-							}
-						}
-//						Console.WriteLine (strReadLine); //屏幕打印每行数据
-//						Console.WriteLine ("tm.Browser=" + tm.Browser);
-//						Console.WriteLine ("tm.Brand=" + tm.Brand);
-//						Console.WriteLine ("tm.Model=" + tm.Model);
-//						Console.WriteLine ("tm.Platform=" + tm.Platform);
-//						Console.WriteLine ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-//						Console.WriteLine("++++>" + sw.ElapsedMilliseconds);
-					} else {
-//						Console.WriteLine (strReadLine); //屏幕打印每行数据
-//						Console.WriteLine("---->" + sw.ElapsedMilliseconds);
-					}
-				}
+                        if (tm.Browser.Equals("UCBrowser"))
+                        {
+                            ucUser++;
+                            Console.WriteLine(strReadLine); //屏幕打印每行数据
+                            sArray = strReadLine.Split('\t');
+                            if (sArray[1].Length > 0)
+                            {
+                                int result = int.Parse(sArray[1].ToString());
+                                Console.WriteLine("UC+" + result);
+                                ucnum += result;
+                            }
+                        }
+                        //						Console.WriteLine (strReadLine); //屏幕打印每行数据
+                        //						Console.WriteLine ("tm.Browser=" + tm.Browser);
+                        //						Console.WriteLine ("tm.Brand=" + tm.Brand);
+                        //						Console.WriteLine ("tm.Model=" + tm.Model);
+                        //						Console.WriteLine ("tm.Platform=" + tm.Platform);
+                        //						Console.WriteLine ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+                        //						Console.WriteLine("++++>" + sw.ElapsedMilliseconds);
+                    }
+                    else
+                    {
+                        //						Console.WriteLine (strReadLine); //屏幕打印每行数据
+                        //						Console.WriteLine("---->" + sw.ElapsedMilliseconds);
+                    }
+                }
 
-			}
-			//关闭读取流文件
-			srReadFile.Close();
-			Console.WriteLine("total:" + total);
-			Console.WriteLine("haveNum:" + haveNum);
-			Console.WriteLine("PER:" + haveNum/total*100+"%");
-			Console.WriteLine("ucUser:" + ucUser);
-			Console.WriteLine("UC:" + ucnum);
-			Console.WriteLine("总:" + allnum);
->>>>>>> 4c44848d1d0410d90828d1021f224b6c626283e6
+            }
+            //关闭读取流文件
+            srReadFile.Close();
+            Console.WriteLine("total:" + total);
+            Console.WriteLine("haveNum:" + haveNum);
+            Console.WriteLine("PER:" + haveNum / total * 100 + "%");
+            Console.WriteLine("ucUser:" + ucUser);
+            Console.WriteLine("UC:" + ucnum);
+            Console.WriteLine("总:" + allnum);
         }
 
         static void uap_OnProcessUnknowUa(string uaStr)
